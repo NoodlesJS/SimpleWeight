ff225337138700428f03c94d29dba587209cfacc
NoodlesJS <romaenwritescode@gmail.com>
2021-08-29 14:56:51 -0500

    added in plus button

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import Home from './home';
 import Statistics from './statistics';
 import { colors, type } from '../theme';
 import Home_icon from '../assets/images/home_icon.png'
 import Statistics_icon from '../assets/images/statistics_icon.png'
+import Plus_icon from '../assets/images/plus.png'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -23,8 +24,54 @@ const Icon = ({source, focused, name}) => {
     )
 }
 
-const FABnull = () => {
+const PlusIcon = () => {
+    return (
+        <View style={{position: 'relative', justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{
+                height:24,
+                width: 3,
+                borderRadius: 50,
+                backgroundColor: 'white',
+            }}></View>
+            <View style={{
+                height:3,
+                width:24,
+                borderRadius: 50,
+                backgroundColor: 'white',
+                position: 'absolute',
+                left: -10.5
+            }}></View>
+        </View>
+    )
+}
+
+const FABnullScreen = () => {
     return null;
+}
+
+const FAB = ({children, onPress}) => {
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                top: -15
+            }}
+            activeOpacity={0.8}
+        >
+            <View
+                style={{
+                    width: 70,
+                    height: 70,
+                    backgroundColor: colors.primary,
+                    borderRadius: 50
+                }}
+            >
+                {children}
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 export default function main() {
@@ -34,7 +81,12 @@ export default function main() {
                 screenOptions={{
                     tabBarShowLabel: false,
                     headerShown: false,
-                    tabBarStyle: { height: 60, borderTopStartRadius: 20, borderTopEndRadius: 20, borderTopWidth: 0}
+                    tabBarStyle: { 
+                        height: 65, 
+                        borderTopStartRadius: 30, 
+                        borderTopEndRadius: 30, 
+                        borderTopWidth: 0
+                    }
                 }}
             >
                 <Tab.Screen 
@@ -49,7 +101,21 @@ export default function main() {
                 />
                 <Tab.Screen
                     name="Action"
-                    component={FABnull}
+                    component={FABnullScreen}
+                    options ={{
+                        tabBarIcon: ({focused}) => (
+                            <PlusIcon />
+                        ),
+                        tabBarButton: (props) => (
+                            <FAB {...props} />
+                        ),
+                    }}
+                    listeners={{
+                        tabPress: (e) => {
+                            e.preventDefault();
+                            alert('pressed plus button')
+                        }
+                    }}
                 />
                 <Tab.Screen 
                     name="Statistics" 
