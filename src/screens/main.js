cc5a7eb019cad33c1f984c871ce4f64c092de458
NoodlesJS <romaenwritescode@gmail.com>
2021-08-30 21:41:54 -0500

    bottom sheet there

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useCallback, useMemo, useRef } from 'react'
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import Home from './home';
 import Statistics from './statistics';
 import { colors, type } from '../theme';
 import Home_icon from '../assets/images/home_icon.png'
 import Statistics_icon from '../assets/images/statistics_icon.png'
-import Plus_icon from '../assets/images/plus.png'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {  BottomSheetModal,  BottomSheetModalProvider,} from '@gorhom/bottom-sheet';
 
 const Tab = createBottomTabNavigator();
 
@@ -75,59 +75,77 @@ const FAB = ({children, onPress}) => {
 }
 
 export default function main() {
+    const bottomSheetModalRef = useRef(null);
+    const snapPoints = useMemo(() => ['25%', '50%'], []);
+    const openModal = () => {
+        bottomSheetModalRef.current?.present();
+      }
     return (
-        <View style={{flex: 1}}>
-            <Tab.Navigator
-                screenOptions={{
-                    tabBarShowLabel: false,
-                    headerShown: false,
-                    tabBarStyle: { 
-                        height: 65, 
-                        borderTopStartRadius: 30, 
-                        borderTopEndRadius: 30, 
-                        borderTopWidth: 0
-                    }
-                }}
-            >
-                <Tab.Screen 
-                    name="Home" 
-                    component={Home}
-                    options={{
-                        tabBarIcon: ({focused}) => (
-                            <Icon source={Home_icon} focused={focused} name={`Home`} />
-                        )
-                    }} 
-                
-                />
-                <Tab.Screen
-                    name="Action"
-                    component={FABnullScreen}
-                    options ={{
-                        tabBarIcon: ({focused}) => (
-                            <PlusIcon />
-                        ),
-                        tabBarButton: (props) => (
-                            <FAB {...props} />
-                        ),
-                    }}
-                    listeners={{
-                        tabPress: (e) => {
-                            e.preventDefault();
-                            alert('pressed plus button')
+        <BottomSheetModalProvider>
+            <View style={{flex: 1}}>
+                <Tab.Navigator
+                    screenOptions={{
+                        tabBarShowLabel: false,
+                        headerShown: false,
+                        tabBarStyle: { 
+                            height: 65, 
+                            borderTopStartRadius: 30, 
+                            borderTopEndRadius: 30, 
+                            borderTopWidth: 0
                         }
                     }}
-                />
-                <Tab.Screen 
-                    name="Statistics" 
-                    component={Statistics}
-                    options={{
-                        tabBarIcon: ({focused}) => (
-                            <Icon source={Statistics_icon} focused={focused} name={`Statistics`} />
-                        )
-                    }} 
-                />
-            </Tab.Navigator>
-            
-        </View>
+                >
+                    <Tab.Screen 
+                        name="Home" 
+                        component={Home}
+                        options={{
+                            tabBarIcon: ({focused}) => (
+                                <Icon source={Home_icon} focused={focused} name={`Home`} />
+                            )
+                        }} 
+                    
+                    />
+                    <Tab.Screen
+                        name="Action"
+                        component={FABnullScreen}
+                        options ={{
+                            tabBarIcon: ({focused}) => (
+                                <PlusIcon />
+                            ),
+                            tabBarButton: (props) => (
+                                <FAB {...props} />
+                            ),
+                        }}
+                        listeners={{
+                            tabPress: (e) => {
+                                e.preventDefault();
+                                
+                                openModal()
+                            }
+                        }}
+                    />
+                    <Tab.Screen 
+                        name="Statistics" 
+                        component={Statistics}
+                        options={{
+                            tabBarIcon: ({focused}) => (
+                                <Icon source={Statistics_icon} focused={focused} name={`Statistics`} />
+                            )
+                        }} 
+                    />
+                </Tab.Navigator>
+                <BottomSheetModal
+                    ref={bottomSheetModalRef}
+                    index={1}
+                    snapPoints={snapPoints}
+                >
+                    <View style={{flex: 1}}>
+                        <Text>Some text here</Text>
+                    </View>
+                </BottomSheetModal>
+            </View>
+        </BottomSheetModalProvider>
     )
 }
+
+
